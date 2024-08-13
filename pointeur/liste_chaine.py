f94b9d2140adeee91c71ae498239451ed2f2304d
Aziz <141174883+AzizDevCraft@users.noreply.github.com>
2024-08-13 19:03:20 +0200

    j'ai terminer la fonction ajouter, contain, boucle for, lengh

--- pointeur/liste_chaine.py
+++ pointeur/liste_chaine.py
@@ -19,7 +19,7 @@ class ListeChainee :
     
     def __str__ (self) : 
         if self.tete == None :
-            return None 
+            return None
         current = self.tete
         ch = f"{current.valeur} -> "
         while current.suivant != None : 
@@ -28,19 +28,38 @@ class ListeChainee :
         return ch [:-4]
                     
     def ajouter (self, valeur) : 
-        pass
+        ele = Noeud (valeur)
+        if self.tete == None : 
+            self.tete = ele
+        else : 
+            current = self.tete 
+            while current.suivant != None : 
+                current = current.suivant     
+            current.suivant = ele
     
     def __iter__ (self) : 
-        pass
-    
+        if self.tete != None :  
+            current = self.tete
+            while current != None : 
+                yield current.valeur
+                current = current.suivant 
+           
     def __contains__ (self, value) :
-        pass 
-    
-    def __del__ (self, cellule) : 
-        pass 
+        if self.tete != None : 
+            if self.tete.valeur == value : 
+                return True
+            current = self.tete
+            while current.suivant != None : 
+                current = current.suivant
+                if current.valeur == value : 
+                    return True
+        return False 
 
     def __len__ (self) : 
-        pass 
+        compteur = 0
+        for i in self : 
+            compteur += 1
+        return compteur 
     
     def __add__ (self, plus) : 
         pass 
@@ -54,7 +73,11 @@ class ListeChainee :
 if __name__ == "__main__" :
     chaine = ListeChainee (Noeud (5, Noeud (4, Noeud (3, Noeud (2)))))
     print (chaine)
+    chaine.ajouter (1)
+    print (chaine)
     for i in chaine : 
-        print (i)
-    
-    
+        print (i) 
+    print (5 in chaine)
+    print (9 in chaine)
+    print (1 in chaine)
+    print (len(chaine))
