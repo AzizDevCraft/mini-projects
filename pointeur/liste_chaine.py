f9225a6ab4cb94f4f8b3a2cdbb31864afa4d05e9
Aziz <141174883+AzizDevCraft@users.noreply.github.com>
2024-08-13 18:29:28 +0200

    ajout du fichier test et marquer les fcts manquantes

--- pointeur/liste_chaine.py
+++ pointeur/liste_chaine.py
@@ -4,6 +4,7 @@
             vers le nœud suivant. """
             
 class Noeud : 
+    
     def __init__ (self, valeur, suivant = None) : 
         self.valeur = valeur 
         self.suivant = suivant 
@@ -12,8 +13,48 @@ class Noeud :
         return repr ((self.valeur, self.suivant))
 
 class ListeChainee : 
-    pass
+    
+    def __init__ (self, tete = None) : 
+        self.tete = tete
+    
+    def __str__ (self) : 
+        if self.tete == None :
+            return None 
+        current = self.tete
+        ch = f"{current.valeur} -> "
+        while current.suivant != None : 
+            current = current.suivant
+            ch += f"{current.valeur} -> "
+        return ch [:-4]
+                    
+    def ajouter (self, valeur) : 
+        pass
+    
+    def __iter__ (self) : 
+        pass
+    
+    def __contains__ (self, value) :
+        pass 
+    
+    def __del__ (self, cellule) : 
+        pass 
 
-if __name__ == "__main__" : 
-    chaine = Noeud (5)
+    def __len__ (self) : 
+        pass 
+    
+    def __add__ (self, plus) : 
+        pass 
+    
+    def reverse (self) : 
+        pass 
+    
+    def sort (self) : 
+        pass
+   
+if __name__ == "__main__" :
+    chaine = ListeChainee (Noeud (5, Noeud (4, Noeud (3, Noeud (2)))))
     print (chaine)
+    for i in chaine : 
+        print (i)
+    
+    
