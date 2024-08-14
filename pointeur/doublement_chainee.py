fa8b1c6226b413ce1e5f1710e7b1816823e619af
Aziz <141174883+AzizDevCraft@users.noreply.github.com>
2024-08-14 08:49:48 +0200

    j'ai défini tout les fonction que je doit faire

--- pointeur/doublement_chainee.py
+++ pointeur/doublement_chainee.py
@@ -9,4 +9,48 @@ class ListDoublementChainee :
     
     def __init__ (self) :
         self.tete = None 
-        self.queue = None  
+        self.queue = None 
+        
+    def __str__ (self) : 
+        if self.tete == None :
+            return repr(None)
+        ch = ''
+        for i in self : 
+            ch += f"{i} -> "
+        return ch [:-4] 
+    
+    def __iter__ (self) : 
+        pass 
+    
+    def __contains__ (self, value) : 
+        pass 
+    
+    def __len__ (self) : 
+        pass 
+    
+    def __add__ (self) : 
+        pass
+    
+    def __radd__ (self) : 
+        pass 
+    
+    def __getitem__ (self, index) : 
+        pass 
+    
+    def __setitem__ (self, index, new) : 
+        pass
+    
+    def __delitem__ (self, index) : 
+        pass 
+    
+    def ajouter (self, valeur) : 
+        pass
+    
+    def insertion (self, index, valeur) : 
+        pass 
+    
+    def find (self, item) : 
+        pass 
+    
+    def reverse (self) : 
+        pass 
