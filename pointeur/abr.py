2daf67543addf20f25344eaf3becde74bf74ffcd
Aziz <141174883+AzizDevCraft@users.noreply.github.com>
2024-08-15 22:28:58 +0200

    j'ai définit tout les fcts que je dois faire

--- pointeur/abr.py
+++ pointeur/abr.py
@@ -13,3 +13,36 @@ class Abr :
     def __str__ (self) : 
         pass 
     
+    def __iter__ (self) : 
+        """parcourt infixe"""
+    
+    
+    def find (self, item) :
+        pass 
+    
+    def __contains__ (self, item) :
+        pass
+    
+    def maxi (self, noeud) : 
+        pass
+    
+    def mini (self, noeud) : 
+        pass 
+    
+    def sucesseur (self) : 
+        pass 
+    
+    def supression (self) : 
+        pass 
+    
+    def mirroir (self) : 
+        pass 
+    
+    def sum_feuille (self) : 
+        pass 
+    
+    def sum_noeud (self) : 
+        pass 
+    
+    def is_abr (self) : 
+        pass 
