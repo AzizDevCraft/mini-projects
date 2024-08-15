31bf05b17ed96b2c8b8dcb35adc9a46f2059bf0c
Aziz <141174883+AzizDevCraft@users.noreply.github.com>
2024-08-15 22:20:05 +0200

    first commit

---
+++ pointeur/abr.py
@@ -0,0 +1,15 @@
+class cellule : 
+    def __init__ (self, valeur) : 
+        self.valeur = valeur
+        self.gauche = None 
+        self.droite = None 
+        self.pere = None 
+        
+class Abr : 
+    
+    def __init__ (self, racine = None) : 
+        self.racine = racine
+        
+    def __str__ (self) : 
+        pass 
+    
