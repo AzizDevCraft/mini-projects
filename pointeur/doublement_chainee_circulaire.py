83cc7aab9f53b4b154946db81966f665ee90b96d
Aziz <141174883+AzizDevCraft@users.noreply.github.com>
2024-08-16 21:50:52 +0200

    first commit

---
+++ pointeur/doublement_chainee_circulaire.py
@@ -0,0 +1,53 @@
+class Noeud : 
+    
+    def __init__ (self, valeur) : 
+        self.valeur = valeur 
+        self.suivant = None 
+        self.prec = None 
+
+class ListDoublementChaineeCirculaire : 
+    
+    def __init__ (self, tete = None) : 
+        self.tete = None 
+        
+    def __str__ (self) : 
+        pass
+    
+    def __iter__ (self) : 
+        pass
+    
+    def __contains__ (self, item) : 
+        pass 
+    
+    def __len__ (self) : 
+        pass
+    
+    def __add__ (self, plus) : 
+        pass
+    
+    def _radd__ (self, plus) : 
+        pass
+    
+    def __getitem__ (self, index) : 
+        pass
+    
+    def __setitem__ (self, index, new) : 
+        pass
+    
+    def __delitem__ (self, index) : 
+        pass
+    
+    def ajouter (self, value) : 
+        pass
+    
+    def insert (self, value, index = 0) : 
+        pass
+    
+    def find (self, value) : 
+        pass 
+    
+    def reverse (self) : 
+        pass 
+    
+if __name__ == "__main__" : 
+    """espace reserve au testes !"""
