a0c6283ad594e10cf54afcbd08b0b0138546f507
Aziz <141174883+AzizDevCraft@users.noreply.github.com>
2025-02-01 10:43:49 +0100

    push methode done

--- data-struture/pile.py
+++ data-struture/pile.py
@@ -1,4 +1,4 @@
-# stack 
+# pile = stack 
 from dataclasses import dataclass
 
 @dataclass
@@ -18,10 +18,27 @@ class Stack :
         return self.top is None
     
     def push (self, value : int) :
-        pass
+        node = Node (value)
+        if self.top is not None : 
+            self.top, self.top.next = node, self.top 
+        else : 
+            self.top = node    
+        self.size += 1
     
     def pop (self) -> "Node" : 
         pass
+            
     
     def peek (self) -> "Node" : 
         pass  
+    
+if __name__ == "__main__" : 
+    pile = Stack ()
+    
+    print (pile.is_empty())
+    pile.push (5)
+    pile.push (10)
+    pile.push (1)
+    print (pile)
+    print (len (pile))
+    print (pile.is_empty ())
