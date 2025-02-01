b06820a921edf544dc2d6a9c032f6486e681fa6e
Aziz <141174883+AzizDevCraft@users.noreply.github.com>
2025-02-01 12:20:34 +0100

    les différents methode à creer

--- data-struture/file.py
+++ data-struture/file.py
@@ -2,3 +2,28 @@
 from dataclasses import dataclass 
 from typing import Union 
 
+@dataclass
+class Node : 
+    value : int
+    next : "Node" = None
+    
+@dataclass 
+class Queue : 
+    tete : "Node" = None
+    queue : "Node" = None
+    size : int = 0
+    
+    def __len__ (self) -> int : 
+        return self.size
+    
+    def enqueue (self, value) : 
+        pass 
+    
+    def dequeue (self) -> "Node" : 
+        pass
+    
+    def peek (self) -> Union [int, None] : 
+        pass 
+    
+    def is_empty (self) -> bool :
+        pass
