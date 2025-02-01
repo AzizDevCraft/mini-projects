8238f4f3be6c4301e693a946761ede15c4a95fdd
Aziz <141174883+AzizDevCraft@users.noreply.github.com>
2025-02-01 10:34:00 +0100

    initialisation des fonctions

--- data-struture/pile.py
+++ data-struture/pile.py
@@ -1,1 +1,27 @@
 # stack 
+from dataclasses import dataclass
+
+@dataclass
+class Node : 
+    value : int
+    next : "Node" = None 
+    
+@dataclass
+class Stack : 
+    top : "Node" = None 
+    size : int = 0
+    
+    def __len__ (self) -> int : 
+        return self.size
+    
+    def is_empty (self) -> bool : 
+        return self.top is None
+    
+    def push (self, value : int) :
+        pass
+    
+    def pop (self) -> "Node" : 
+        pass
+    
+    def peek (self) -> "Node" : 
+        pass  
